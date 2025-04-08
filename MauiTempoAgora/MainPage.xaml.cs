b3168c28c42d0311adffe156c0ddf94f56521ac7
Slyyd <windows7kaue@gmail.com>
2025-04-08 20:54:25 -0300

    Adicionado localização em tempo real e mapa do site Windy.com

--- MauiTempoAgora/MainPage.xaml.cs
+++ MauiTempoAgora/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using MauiTempoAgora.Models;
+﻿
+using MauiTempoAgora.Models;
 using MauiTempoAgora.Services;
+using System.Linq.Expressions;
 
 namespace MauiTempoAgora
 {
@@ -23,25 +25,81 @@ namespace MauiTempoAgora
 
                     Tempo? t = await DataService.GetWeather(ent_cidade.Text);
 
-                    if (t != null) 
+                    if (t != null)
                     {
 
                         lst_Legal = t;
                         BindingContext = lst_Legal;
 
+                        string linkMapa = $"https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+                        windyMap.Source = linkMapa;
+
+
+
                         return;
 
-                    } 
+                    }
 
-                    
+
 
                 }
 
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 await DisplayAlert("Erro!", ex.Message, "OK!");
             }
+        }
+
+        private async void btn_localizacao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest requestLocalizacao = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+
+                Location? localizacaoAtual = await Geolocation.Default.GetLocationAsync(requestLocalizacao);
+                if (localizacaoAtual == null) { throw new Exception("Localização nula!"); }
+
+                GetCidade(localizacaoAtual.Latitude, localizacaoAtual.Longitude);
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro! Dispositivo não suporta GPS!", fnsEx.Message, "OK!");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro! Localização não ativada!", fneEx.Message, "OK!");
+            }
+            catch (PermissionException perEx)
+            {
+                await DisplayAlert("Erro! Permissão negada!", perEx.Message, "OK!");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK!");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+                if (place == null) { throw new Exception("Nenhum local encontrado!"); }
+
+                ent_cidade.Text = place.Locality;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK!");
+            }
+
+
+
         }
     }
 
